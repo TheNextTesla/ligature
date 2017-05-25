e8dba044be0ee43dd3b1ba5fd9924e95a9c11696
TheNextTesla <bhthenexttesla@gmail.com>
2017-05-25 14:26:20 -0400

    Added Primitive Display Package
    Comprised Only of FrameCore at this Time

---
+++ Crescendo/src/display/FrameCore.java
@@ -0,0 +1,13 @@
+package display;
+
+import javax.swing.JFrame;
+
+public class FrameCore extends JFrame
+{
+
+	/**
+	 * Requested by JFrame
+	 */
+	private static final long serialVersionUID = -1140522721030212890L;
+	
+}
