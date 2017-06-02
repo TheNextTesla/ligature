2eaaae1fad4c7b7c3e2476fe077e3bca77302857
tcox18 <tcox18@HS-MATH2-21.gvsd.org>
2017-06-02 14:27:49 -0400

    5 lines we dont really need

--- Crescendo/src/control/TaskMain.java
+++ Crescendo/src/control/TaskMain.java
@@ -1,11 +1,31 @@
 package control;
 
 import display.FrameCore;
+import javacalculus.core.CalcParser;
+import javacalculus.evaluator.CalcPLOT;
+import javacalculus.exception.CalcSyntaxException;
+import javacalculus.struct.CalcFunction;
+import javacalculus.struct.CalcObject;
+import javacalculus.struct.CalcSymbol;
 
 public class TaskMain
 {
 	public static void main(String[] args) 
 	{
 		new FrameCore();
+
+		try
+		{
+			String function = "sin(x)";
+			CalcParser cvaluate = new CalcParser();
+			CalcObject cobject = cvaluate.parse(function);
+			CalcPLOT cplot = new CalcPLOT();
+			cplot.evaluate(new CalcFunction(new CalcSymbol("x"), cobject));
+			System.out.println(cplot.getYValue(1));
+		}
+		catch(CalcSyntaxException cse)
+		{
+			cse.printStackTrace();
+		}
 	}
 }
