29749636e3898d6a720c46dc786360dbd080bd0b
sonofcpx2 <sonofcpx2@TAC-NV.hsd1.pa.comcast.net>
2017-06-07 17:04:20 -0400

    IT WORKS!!! WITH A GRAPH TOO YAY!

--- Crescendo/src/music/MusicHandler.java
+++ Crescendo/src/music/MusicHandler.java
@@ -2,10 +2,12 @@ package music;
 
 import javacalculus.core.CalcParser;
 import javacalculus.core.CalculusEngine;
+import javacalculus.evaluator.CalcPLOT;
 import javacalculus.evaluator.CalcSUB;
 import javacalculus.evaluator.extend.CalcNullEvaluator;
 import javacalculus.exception.CalcSyntaxException;
 import javacalculus.struct.CalcDouble;
+import javacalculus.struct.CalcFunction;
 import javacalculus.struct.CalcObject;
 import javacalculus.struct.CalcSymbol;
 
@@ -16,8 +18,25 @@ public class MusicHandler
 	
 	public MusicHandler(String instring)
 	{
-		string = instring;
-		values = new double[21];					
+		string = fixcase(instring);
+		values = new double[21];
+	}
+	
+	private String fixcase(String instring)
+	{
+		String returnstring = "";
+		for(int i =0; i<instring.length(); i++)
+		{
+			if(instring.substring(i, i+1).equals("x"))
+			{
+				returnstring += instring.substring(i,i+1);
+			}
+			else
+			{
+				returnstring += instring.substring(i,i+1).toUpperCase();
+			}
+		}
+		return returnstring;
 	}
 	
 	public void evaluateCalc()
@@ -42,9 +61,14 @@ public class MusicHandler
 		if(parsed == null || evaluated == null)
 		{
 			System.out.println("Error - Parse Failed");
+			System.out.println(string);
 			return;
 		}
 		
+		
+		CalcObject tempobject = new CalcFunction(new CalcSymbol("calcsymbol", new CalcNullEvaluator(), 0x0400), evaluated, new CalcSymbol("x", new CalcNullEvaluator(), 0x0400));
+		CalcPLOT graph = new CalcPLOT();
+		graph.evaluate((CalcFunction) tempobject);
 		
 		for(int i = 0; i < values.length; i++)
 		{
